bf005b6b01dcd79a2e370dacb973238ec426fda5
jimao <shihao.gu@perfma.com>
2025-01-20 20:00:16 +0800

    feat: first dialogue

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+import { ChatOllama } from '@langchain/ollama';
 
-console.log('你好，世界');
+async function main() {
+  const model = new ChatOllama({
+    baseUrl: 'http://183.220.36.102:31351',
+    model: 'phi4:latest',
+  });
+  const result = await model.invoke(["human", "你好"]);
+  console.log(result.content);
+}
+
+main();

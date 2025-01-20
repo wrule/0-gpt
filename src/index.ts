aa99521ba21f19c9b8e09ee66739db4acd75d8cb
jimao <shihao.gu@perfma.com>
2025-01-20 20:05:19 +0800

    feat: 简单翻译器

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { ChatOllama } from '@langchain/ollama';
 
-async function main() {
+async function translate(content: string, source: string, target: string) {
   const model = new ChatOllama({
     baseUrl: 'http://183.220.36.102:31351',
     model: 'phi4:latest',
   });
-  const result = await model.invoke(["human", "你好"]);
+  const result = await model.invoke(["human", `请帮我把[${content}]这段文本由(${source})翻译成为(${target})，翻译后的文本放在[]之间`]);
   console.log(result.content);
+}
+
+async function main() {
+  translate('我目前在杭州笨马网络技术有限公司工作', '汉语', '英语');
 }
 
 main();

c310fe3603285ae9723cdb90062c534d8fc60b4d
jimao <shihao.gu@perfma.com>
2025-01-21 16:18:52 +0800

    feat: 文档回复

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { ChatOllama } from '@langchain/ollama';
 import docs from './docs';
 
+console.log(docs.length);
+
 async function agentServe() {
   const model = new ChatOllama({
     baseUrl: 'http://183.220.36.102:31351',
@@ -15,7 +17,7 @@ async function agentServe() {
     { role: 'system', content: '下面是XSea性能测试平台的文档。你需要以性能测试为背景，深入理解这些文档的内容，并以此文档为基础解答用户的使用问题或者概念性问题' },
     { role: 'system', content: `这是文档内容：\n\n${docs}\n\n` },
     { role: 'system', content: '避免提及你是从上下文中获取的信息，并根据用户问题的语言来回答。如果你不知道，就直说你不知道。如果你不确定，就寻求澄清' },
-    { role: 'user', content: '脚本有哪些' },
+    { role: 'user', content: '怎么配置熔断规则' },
   ]);
   return result.content;
 }
